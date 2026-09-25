5e4ccc173a0c7a35c0531b4c82ee2a2d82d5a8bc
Goncalo-Lee <gonl33@proton.me>
2026-09-25 21:09:26 +0100

    Add Input UI (Need to finished)

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,17 +1,38 @@
-import * as React from "react"
-import { Input as InputPrimitive } from "@base-ui/react/input"
-import { cn } from "cn"
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
-export function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+const inputVariants = cva(
+  "mt-3 block w-full h-12 rounded-lg bg-white px-4 text-gray-800 shadow-soft outline-none focus:ring-2 focus:ring-brand/60",
+  {
+    variants: {
+      variant: {
+        primary: "",
+      },
+      size: {
+        sm: "/* TU */",
+        md: "h-12 text-lg",
+        lg: "h-14",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  },
+);
+
+type InputProps = React.ComponentProps<"input"> &
+  VariantProps<typeof inputVariants>;
+
+export function Input({ className, variant, size, ...props }: InputProps) {
   return (
-    <InputPrimitive
-      type={type}
-      data-slot="input"
-      className={cn(
-        "h-8 w-full min-w-0 rounded-lg border border-input bg-transparent px-2.5 py-1 text-base transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
-        className
-      )}
-      {...props}
+    <input
+      id=""
+      name=""
+      type=""
+      autoComplete=""
+      required
+      className={cn(inputVariants({ variant, size }), className)}
     />
   )
 }
